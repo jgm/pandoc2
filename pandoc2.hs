1b914df76a1d9efea2ae8f917ced18a821c7d16f
John MacFarlane <jgm@berkeley.edu>
2011-05-01 18:15:18 -0700

    Added more inline elements.

--- pandoc2.hs
+++ pandoc2.hs
@@ -7,9 +7,51 @@ import qualified Data.Text as T
 import Data.Text (Text)
 import Data.Data
 import Data.List (intersperse)
-import Data.Foldable (toList, foldMap)
-import qualified Text.ParserCombinators.ReadP as R
+import Data.Foldable (toList)
 import Data.Generics
+
+data Inline = Txt Text
+            | Sp
+            | Emph Inlines
+            | Strong Inlines
+            | SmallCaps Inlines
+            | Underline Inlines
+            | Strike Inlines
+            | Superscript Inlines
+            | Subscript Inlines
+            | Image Label Title Source
+            | Link Label Title Source
+            | Verbatim Attr Text
+            | LineBreak
+            | RawInline Format Text
+            | Note
+            deriving (Show, Read, Data, Ord, Eq, Typeable)
+
+newtype Label = Label Inlines
+              deriving (Show, Read, Data, Ord, Eq, Typeable)
+
+newtype Title = Title Text
+              deriving (Show, Read, Data, Ord, Eq, Typeable)
+
+newtype Source = Source Text
+              deriving (Show, Read, Data, Ord, Eq, Typeable)
+
+newtype Format = Format Text
+               deriving (Show, Read, Data, Typeable)
+
+instance Eq Format where
+  Format x == Format y = T.toUpper x == T.toUpper y
+
+instance Ord Format where
+  Format x `compare` Format y = T.toUpper x `compare` T.toUpper y
+
+data Attr = Attr { attrId      :: Text
+                 , attrClasses :: [Text]
+                 , attrKeyVals :: [(Text, Text)] }
+                 deriving (Show, Read, Data, Ord, Eq, Typeable)
+
+(<>) :: Monoid a => a -> a -> a
+(<>) = mappend
 
 newtype Inlines = Inlines { unInlines :: Seq Inline }
                 deriving (Data, Ord, Eq, Typeable)
@@ -30,12 +72,20 @@ instance Monoid Inlines where
       (xs' :> x, y :< ys') -> Inlines (meld `mappend` ys')
         where meld = case (x, y) of
                           (Sp, Sp)           -> xs' |> Sp
-                          (Txt t1, Txt t2)   -> xs' |> Txt (t1 `mappend` t2)
-                          (Emph i1, Emph i2) -> xs' |> Emph (i1 `mappend` i2)
+                          (Txt t1, Txt t2)   -> xs' |> Txt (t1 <> t2)
+                          (Emph i1, Emph i2) -> xs' |> Emph (i1 <> i2)
+                          (Strong i1, Strong i2) -> xs' |> Strong (i1 <> i2)
+                          (SmallCaps i1, SmallCaps i2) ->
+                                                xs' |> SmallCaps (i1 <> i2)
+                          (Underline i1, Underline i2) ->
+                                                xs' |> Underline (i1 <> i2)
+                          (Strike i1, Strike i2) -> xs' |> Strike (i1 <> i2)
+                          (Superscript i1, Superscript i2) ->
+                                                xs' |> Superscript (i1 <> i2)
+                          (Subscript i1, Subscript i2) ->
+                                                xs' |> Subscript (i1 <> i2)
+                          (Sp, LineBreak)    -> xs' |> LineBreak
                           _                  -> xs' |> x |> y
-
-(<>) :: Monoid a => a -> a -> a
-(<>) = mappend
 
 newtype Blocks = Blocks { unBlocks :: Seq Block }
                 deriving (Data, Ord, Eq, Typeable)
@@ -46,11 +96,6 @@ instance Show Blocks where
 
 instance Read Blocks where
   readsPrec n = map (\(x,y) -> (Blocks . fromList $ x, y)) . readsPrec n
-
-data Inline = Txt Text
-            | Sp
-            | Emph Inlines
-            deriving (Show, Read, Data, Ord, Eq, Typeable)
 
 data Block = Para Inlines
            deriving (Show, Read, Data, Ord, Eq, Typeable)
@@ -66,6 +111,9 @@ literalText = Inlines . singleton . Txt
 emph :: Inlines -> Inlines
 emph = Inlines . singleton . Emph
 
+link :: Inlines -> Text -> Text -> Inlines
+link lab tit src = Inlines $ singleton $ Link (Label lab) (Title tit) (Source src)
+
 -- just testing generics:
 
 bottomUp f = everywhere (mkT f)
@@ -73,6 +121,9 @@ bottomUp f = everywhere (mkT f)
 gentest :: Inline -> Inline
 gentest (Txt t) = Txt $ T.toUpper t
 gentest x = x
+
+gentest2 :: Title -> Title
+gentest2 (Title x) = (Title "")
 
 --
 
