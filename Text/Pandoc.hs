b269c1587bde5e7bbbe65ea965d1294bb5b31b71
John MacFarlane <jgm@berkeley.edu>
2011-05-11 20:04:21 -0700

    Added Parsing module to Text.Pandoc.

--- Text/Pandoc.hs
+++ Text/Pandoc.hs
@@ -1,9 +1,11 @@
 module Text.Pandoc ( module Text.Pandoc.Definition
                    , module Text.Pandoc.Builder
+                   , module Text.Pandoc.Parsing
                    , module Text.Pandoc.Reader.Markdown
                    , module Text.Pandoc.Writer.HTML )
 where
 import Text.Pandoc.Definition
 import Text.Pandoc.Builder
+import Text.Pandoc.Parsing
 import Text.Pandoc.Reader.Markdown
 import Text.Pandoc.Writer.HTML
