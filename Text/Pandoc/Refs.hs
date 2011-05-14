ecd8b5d8c9004a723e5dbf81c2029838d344a48a
John MacFarlane <jgm@berkeley.edu>
2011-05-14 16:50:21 -0700

    Added note lookup to resolveRefs

--- Text/Pandoc/Refs.hs
+++ Text/Pandoc/Refs.hs
@@ -7,21 +7,19 @@ import qualified Data.Map as M
 import qualified Data.Foldable as F
 import Text.Parsec (getState)
 import Control.Applicative ((<$>))
+import Data.Monoid
 
 -- Reference resolution
 
 resolveRefs :: PMonad m => Blocks -> P m Blocks
 resolveRefs bs = do
-  refs <- sReferences <$> getState
-  return $ handleRefs refs bs
+  st <- getState
+  return $ F.foldMap (handleRefB st) $ unBlocks bs
 
-handleRefs :: M.Map Key Source -> Blocks -> Blocks
-handleRefs refs = F.foldMap (handleRefB refs) . unBlocks
-
-handleRefB :: M.Map Key Source -> Block -> Blocks
-handleRefB refs x =
-  let goI = F.foldMap (handleRefI refs) . unInlines
-      goB = F.foldMap (handleRefB refs) . unBlocks
+handleRefB :: PMonad m => PState m -> Block -> Blocks
+handleRefB st x =
+  let goI = F.foldMap (handleRefI st) . unInlines
+      goB = F.foldMap (handleRefB st) . unBlocks
   in case x of
       (Para ils)       -> para $ goI ils
       (Plain ils)      -> block $ Plain $ goI ils
@@ -30,22 +28,26 @@ handleRefB refs x =
       (Header lev ils) -> header lev $ goI ils
       _                -> block x
 
-handleRefI :: M.Map Key Source -> Inline -> Inlines
-handleRefI refs x =
-  let goI = F.foldMap (handleRefI refs) . unInlines
-      goB = F.foldMap (handleRefB refs) . unBlocks
+handleRefI :: PMonad m => PState m -> Inline -> Inlines
+handleRefI st x =
+  let goI = F.foldMap (handleRefI st) . unInlines
+      goB = F.foldMap (handleRefB st) . unBlocks
   in case x of
       (Emph ils)       -> inline $ Emph $ goI ils
       (Strong ils)     -> inline $ Strong $ goI ils
       (Quoted qt ils)  -> inline $ Quoted qt $ goI ils
       (Link (Label lab) Ref{ key = k, fallback = ils }) ->
-        case M.lookup k refs of
+        case M.lookup k (sReferences st) of
              Just s  -> inline $ Link (Label $ goI lab) s
              Nothing -> goI ils
       (Image (Label lab) Ref{ key = k, fallback = ils }) ->
-        case M.lookup k refs of
+        case M.lookup k (sReferences st) of
              Just s  -> inline $ Image (Label $ goI lab) s
              Nothing -> goI ils
-      (Note k bs)      -> inline $ Note k $ goB bs
+      (Note (Key k) bs) | k == mempty -> inline $ Note (Key mempty) $ goB bs
+      (Note (Key k) _) ->
+        case M.lookup (Key k) (sNotes st) of
+             Just bs -> inline $ Note (Key mempty) $ goB bs
+             Nothing -> goI k
       _                -> inline x
 
