0849e03e07c403943fa1db05428c644cb41f48e9
John MacFarlane <jgm@berkeley.edu>
2011-05-14 17:21:54 -0700

    Made handleRef{B/I} monadic.

--- Text/Pandoc/Refs.hs
+++ Text/Pandoc/Refs.hs
@@ -12,42 +12,45 @@ import Data.Monoid
 -- Reference resolution
 
 resolveRefs :: PMonad m => Blocks -> P m Blocks
-resolveRefs bs = do
+resolveRefs bs = F.foldlM handleRefB mempty $ unBlocks bs
+
+handleRefB :: PMonad m => Blocks -> Block -> P m Blocks
+handleRefB acc x = do
+  let goI = F.foldlM handleRefI mempty . unInlines
+  let goB = F.foldlM handleRefB mempty . unBlocks
+  res <- case x of
+            (Para ils)       -> para <$> goI ils
+            (Plain ils)      -> block . Plain <$> goI ils
+            (Quote bs)       -> quote <$> goB bs
+            (List attr its)  -> block . List attr <$> mapM goB its
+            (Header lev ils) -> header lev <$> goI ils
+            _                -> return $ block x
+  return $ acc <> res
+
+handleRefI :: PMonad m => Inlines -> Inline -> P m Inlines
+handleRefI acc x = do
   st <- getState
-  return $ F.foldMap (handleRefB st) $ unBlocks bs
-
-handleRefB :: PMonad m => PState m -> Block -> Blocks
-handleRefB st x =
-  let goI = F.foldMap (handleRefI st) . unInlines
-      goB = F.foldMap (handleRefB st) . unBlocks
-  in case x of
-      (Para ils)       -> para $ goI ils
-      (Plain ils)      -> block $ Plain $ goI ils
-      (Quote bs)       -> quote $ goB bs
-      (List attr its)  -> block $ List attr $ map goB its
-      (Header lev ils) -> header lev $ goI ils
-      _                -> block x
-
-handleRefI :: PMonad m => PState m -> Inline -> Inlines
-handleRefI st x =
-  let goI = F.foldMap (handleRefI st) . unInlines
-      goB = F.foldMap (handleRefB st) . unBlocks
-  in case x of
-      (Emph ils)       -> inline $ Emph $ goI ils
-      (Strong ils)     -> inline $ Strong $ goI ils
-      (Quoted qt ils)  -> inline $ Quoted qt $ goI ils
-      (Link (Label lab) Ref{ key = k, fallback = ils }) ->
-        case M.lookup k (sReferences st) of
-             Just s  -> inline $ Link (Label $ goI lab) s
-             Nothing -> goI ils
-      (Image (Label lab) Ref{ key = k, fallback = ils }) ->
-        case M.lookup k (sReferences st) of
-             Just s  -> inline $ Image (Label $ goI lab) s
-             Nothing -> goI ils
-      (Note (Key k) bs) | k == mempty -> inline $ Note (Key mempty) $ goB bs
-      (Note (Key k) _) ->
-        case M.lookup (Key k) (sNotes st) of
-             Just bs -> inline $ Note (Key mempty) $ goB bs
-             Nothing -> goI k
-      _                -> inline x
-
+  let goI = F.foldlM handleRefI mempty . unInlines
+  let goB = F.foldlM handleRefB mempty . unBlocks
+  res <- case x of
+            (Emph ils)       -> inline . Emph <$> goI ils
+            (Strong ils)     -> inline . Strong <$> goI ils
+            (Quoted qt ils)  -> inline . Quoted qt <$> goI ils
+            (Link (Label lab) Ref{ key = k, fallback = ils }) ->
+              case M.lookup k (sReferences st) of
+                   Just s  -> do lab' <- goI lab
+                                 return $ inline $ Link (Label lab') s
+                   Nothing -> goI ils
+            (Image (Label lab) Ref{ key = k, fallback = ils }) ->
+              case M.lookup k (sReferences st) of
+                   Just s  -> do lab' <- goI lab
+                                 return $ inline $ Image (Label lab') s
+                   Nothing -> goI ils
+            (Note (Key k) bs) | k == mempty ->
+                   inline . Note (Key mempty) <$> goB bs
+            (Note (Key k) _) ->
+              case M.lookup (Key k) (sNotes st) of
+                   Just bs -> inline . Note (Key mempty) <$> goB bs
+                   Nothing -> goI k
+            _                -> return $ inline x
+  return $ acc <> res
