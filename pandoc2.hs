dd7de99eb2028eba465bd61fbff59df1771635c7
John MacFarlane <jgm@berkeley.edu>
2011-05-17 20:58:46 -0700

    Added 'native' output in executable.

--- pandoc2.hs
+++ pandoc2.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 import Text.Pandoc
 import Text.Blaze.Renderer.Utf8
-import Data.ByteString as B
+import qualified Data.ByteString as B
 import System.Environment
 import Data.Text.Encoding (decodeUtf8)
-import Data.Text as T
+import qualified Data.Text as T
+import Data.Char (toLower)
 import System.Console.CmdArgs
 
 main :: IO ()
@@ -19,25 +20,34 @@ main = do
                            , optStrict  = strict opts
                            , optSmart   = smart opts
                            }
-  let convert x = parseWith poptions' pDoc (decodeUtf8 x)
-                  >>= renderHtmlToByteStringIO B.putStr . docToHtml poptions
+  let convert = parseWith poptions' pDoc . decodeUtf8
+  let render = case map toLower (to opts) of
+                    "html"   -> renderHtmlToByteStringIO B.putStr .
+                                 docToHtml poptions
+                    "native" -> print
+                    _        -> error $ "Unknown writer " ++ show (to opts)
   case files opts of
-       [] -> B.getContents >>= convert
-       fs -> mapM_ (\f -> B.readFile f >>= convert) fs
+       [] -> B.getContents >>= convert >>= render
+       fs -> mapM_ (\f -> B.readFile f >>= convert >>= render) fs
 
 data Pandoc2 = Pandoc2
     { tab_stop    :: Int
     , files       :: [FilePath]
     , strict      :: Bool
     , smart       :: Bool
+    , to          :: String
+    , from        :: String
     }
     deriving (Data,Typeable,Show,Eq)
 
 opts = Pandoc2
-    { tab_stop    = 4 &= groupname "Options" &= help "Tab stop"
-    , files       = def &= args &= typ "FILE.."
+    { from        = "markdown" &= typ "FORMAT" &= help "Source format"
+    , to          = "html" &= typ "FORMAT" &= help "Target format"
     , strict      = def &= help "Disable pandoc's markdown extensions"
     , smart       = def &= help "Smart typography"
+    , tab_stop    = 4 &= groupname "Options" &= explicit &= name "tab-stop"
+                      &= help "Tab stop"
+    , files       = def &= args &= typ "FILE.."
     } &=
     program "pandoc2" &=
     verbosity &=
