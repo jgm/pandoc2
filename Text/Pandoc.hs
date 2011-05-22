f122e29763cc52f7cad15389a86f81ac01ec0062
John MacFarlane <jgm@berkeley.edu>
2011-05-22 09:57:51 -0700

    Removed Text.Pandoc.Parsing from Text.Pandoc.

--- Text/Pandoc.hs
+++ Text/Pandoc.hs
@@ -1,13 +1,12 @@
 module Text.Pandoc ( module Text.Pandoc.Definition
                    , module Text.Pandoc.Builder
                    , module Text.Pandoc.Shared
-                   , module Text.Pandoc.Parsing
                    , module Text.Pandoc.Reader.Markdown
-                   , module Text.Pandoc.Writer.HTML )
+                   , module Text.Pandoc.Writer.HTML
+                   )
 where
 import Text.Pandoc.Definition
 import Text.Pandoc.Builder
 import Text.Pandoc.Shared
-import Text.Pandoc.Parsing
 import Text.Pandoc.Reader.Markdown
 import Text.Pandoc.Writer.HTML
